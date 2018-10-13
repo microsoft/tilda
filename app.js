c05ea30440562fc38b1620b45866f42aada1bb78
Amy Zhang <axz@mit.edu>
2018-10-12 22:39:18 -0400

    fix

--- app.js
+++ app.js
@@ -1333,11 +1333,15 @@ server.post('/api/commands', function (req, res, next) {
 			text = "Ended conversation :end:   :small_red_triangle::small_red_triangle::small_red_triangle::small_red_triangle::small_red_triangle:";
 		} else if (args.command.indexOf('~followchannel') !== -1) {
 		
+			console.log(args.text);
 			var parts = args.text.split(' ');
+			console.log(parts);
 			var channel_info = parts[0].split('|');
 			console.log(channel_info);
 			var channel_id = channel_info[0].substring(1, channel_info[0].length);
 			console.log(channel_id);
+			
+			
 			var people = [];
 			var tags = [];
 			for (var i=1; i<parts.length;i++) {
